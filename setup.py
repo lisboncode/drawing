0348d1e77e6ec37a37b834a9e6b760e6acc9b22d
lisboncode <lisboncode@gmail.com>
2025-05-29 15:10:04 +0100

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import os
+
+from setuptools import setup, find_packages
+setup(
+    name="drawing",
+    version="1.0",
+    description="Drawing Distribution Package",
+    packages=find_packages(),
+    include_package_data=True
+)
