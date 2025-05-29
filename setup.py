54f0d961f5d870bf362a62d421ed8f9055f9e5a7
lisboncode <lisboncode@gmail.com>
2025-05-29 15:59:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 setup(
     name="drawing",
-    version="1.0",
+    version="2.0",
     description="Drawing Distribution Package",
     packages=find_packages(),
     include_package_data=True
